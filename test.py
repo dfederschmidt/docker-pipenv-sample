770186eb1f259c211f45fcb7afd2378371bb70a5
Daniel Federschmidt <daniel@federschmidt.xyz>
2018-06-08 19:50:40 +0200

    add test stage

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+import requests as r
+
+class WebServerTests(unittest.TestCase):
+
+    def test_webserver_accessible(self):
+        res = r.get("http://localhost:5000")
+        self.assertTrue(res.text, "Hello World!")
+
+if __name__ == '__main__':
+    unittest.main()
