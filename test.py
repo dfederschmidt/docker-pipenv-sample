d306a66eb2b21f61b54df1d687fdc9fca4b8d6ed
Daniel Federschmidt <daniel@federschmidt.xyz>
2018-06-08 20:17:12 +0200

    set service

--- test.py
+++ test.py
@@ -2,10 +2,14 @@ import unittest
 import requests as r
 
 class WebServerTests(unittest.TestCase):
+    URL = "localhost"
 
     def test_webserver_accessible(self):
-        res = r.get("http://localhost:5000")
+        res = r.get("http://{}:5000".format(URL))
         self.assertTrue(res.text, "Hello World!")
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        WebServerTests.URL = sys.argv.pop()
+
     unittest.main()
